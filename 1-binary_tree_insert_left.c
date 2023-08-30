5ef1e107f440a9bec0eb32ce7128678b83ad6718
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-08-30 18:30:38 +0300

    tasks from 3 to 10 good

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,6 +4,7 @@
  * @binary_tree_insert_left: function that inserts a node as the left-child of another node.
  * @parent: parent is a pointer to the node to insert the left-child in.
  * @value: value is the value to store in the new node.
+ * Return: insert node in left.
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
